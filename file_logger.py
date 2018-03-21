58aa1dfe1874fb2e09e38213d8a6760258cd757e
Emily-Xue Kang <xkang3@dons.usfca.edu>
2018-03-20 17:10:00 -0700

    delete unecessary comments

--- file_logger.py
+++ file_logger.py
@@ -5,24 +5,7 @@ class file_logger(logger):
 	def __init__(self, log_level, filename='file_log.txt'):
 		logger.__init__(self, log_level)
 		self.output = open(filename, 'a+')
-		self.output.close()
-		# message = "0: Starting logger (type = stdout) at log level {}.".format(log_level)
-		# log(self, log_level, message)
-		# message += '\n' + "Enging logger"
-		# f = open('file_log.txt','w')
-		# f.write(message)
-		# f.close()
 
 	def log(self, log_level, message):
 		if log_level <= self.__log_level__:
 			self.output.write(str(log_level) + ': ' + message + '\n')
-		# if log_level <= 3:
-		# 	if log_level == 1:
-		# 		message+= "\nImportant message."
-		# 	elif log_level == 2:
-		# 		message+= "\nImportant message."
-		# 		message+="\nLess important message."
-		# 	elif log_level == 3:
-		# 		message+= "\nImportant message."
-		# 		message+="\nLess important message."
-		# 		message+="\nNot important message."
--- stdout_logger.py
+++ stdout_logger.py
@@ -2,21 +2,8 @@ from logger import logger
 class stdout_logger(logger):
 	def __init__(self, log_level):
 		logger.__init__(self, log_level)
-		# print("0: Starting logger (type = file) at log level {}.".format(log_level))
-		# logwe(self, log_level, message)
-		# print("0: Ending logger.")
-
+		
 	def log(self, log_level, message):
 		if log_level <= self.__log_level__:
 			print(str(log_level) + ": " + message)
-		# if log_level <= 3:
-		# 	if log_level == 1:
-		# 		print("Important message.\n")
-		# 	elif log_level == 2:
-		# 		print("Important message.\n")
-		# 		print("Less important message.\n")
-		# 	elif log_level == 3:
-		# 		print("Important message.\n")
-		# 		print("Less important message.\n")
-		# 		print("Not important message.\n")
 
