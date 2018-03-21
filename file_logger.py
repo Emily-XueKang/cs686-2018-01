cfb4b53ec11afd2e8ed6290dc160d8b7c5c5d0b2
Emily-Xue Kang <xkang3@dons.usfca.edu>
2018-03-20 17:20:57 -0700

    use append

--- file_logger.py
+++ file_logger.py
@@ -1,11 +1,17 @@
 from logger import logger
 class file_logger(logger):
-	#file_logger(3)
-	logger(3)
+	"""
+    Constructor
+    """
 	def __init__(self, log_level, filename='file_log.txt'):
 		logger.__init__(self, log_level)
-		self.output = open(filename, 'a+')
+		self.output = open(filename, 'a')
 
+	"""
+    log
+    Logs the message if log_level is less than or equal to
+    the class' threshold.
+    """
 	def log(self, log_level, message):
 		if log_level <= self.__log_level__:
 			self.output.write(str(log_level) + ': ' + message + '\n')
